bd660bbf5e49080da6b97efac885fb3e07b44f22
Anshul Kashyap <anshulkashyap0650@gmail.com>
2026-02-20 18:50:40 +0530

    connect database with the app

--- src/app.js
+++ src/app.js
@@ -1,24 +1,7 @@
 const express = require("express");
+require("./config/database");
 
 const app = express();
-
-const { adminAuth } = require("./middlewares/auth")
-app.use("/admin", adminAuth)
-
-app.get(
-    "/admin/getAllData",
-    (req, res) => {
-        res.send("Admin get All Data!");
-    }
-)
-
-app.get(
-    "/admin/delete",
-    (req, res) => {
-        res.send("Delete All Data!");
-    }
-)
-
 
 app.listen(3000, ()=>{
     console.log("Server Created!");
--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,13 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-    "mongodb+srv://db_user:GukiTheWorld1234@hello.wd2sx6g.mongodb.net/"
-);
+const connectDB = async () => {
+    await mongoose.connect(
+        "mongodb+srv://db_user:GukiTheWorld1234@hello.wd2sx6g.mongodb.net/"
+    );
+};
+
+connectDB().then(() =>{
+    console.log("Database connection established..");
+}).catch(err => {
+    console.error("Databse cannot be connected");
+})
