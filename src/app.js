ed27aed3de8fbc6e188207ccd0ac5bce8d494f5e
Anshul Kashyap <anshulkashyap0650@gmail.com>
2026-02-23 18:28:18 +0530

    Adding SignUp API

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
+
+app.post("/signup",
+    async (req, res) => {
+        const user = new User({
+            firstName : "Ansh",
+            email : "anshu0650@gmail.com",
+            password : "ansh@123#"
+        });
+        await user.save();
+    }
+);
+
 
 connectDB().then(() =>{
     console.log("Database connection established..");
--- src/models/user.js
+++ src/models/user.js
@@ -22,4 +22,5 @@ const userSchema = mongoose.Schema({
 });
 
 const User = mongoose.model("User", userSchema);
-module.exports = userModel;
+
+module.exports = User;
