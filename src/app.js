4cbed66e0e0e88e86f9c783fcd99786f8d4a5bb3
Anshul Kashyap <anshulkashyap0650@gmail.com>
2026-02-25 18:36:04 +0530

    adding delete api

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ app.get("/user", async (req, res) => {
     catch (err) {
         res.status(400).send("Something Wrong");
     }
-})
+});
 
 // getting all user
 
@@ -42,6 +42,19 @@ app.get("/feed", async (req, res) => {
     try{
         const user = await User.find({});
         res.send(user);
+    }
+
+    catch (err) {
+        res.status(404).send("Something Wrong");
+    }
+});
+
+app.delete("/user", async(req, res) => {
+    const userId = req.body.userId;
+
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        res.send("user deleted successfully!");
     }
 
     catch (err) {
