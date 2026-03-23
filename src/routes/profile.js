a9e3f24b9957c8db01770bda1c311fb313bdf097
Anshul Kashyap <anshulkashyap0650@gmail.com>
2026-03-23 18:21:30 +0530

    updating profile router

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -24,11 +24,11 @@ profileRouter.patch("/profile/edit", userAuth, async(req, res) => {
 
         Object.keys(req.body).forEach(key => (loggedInUser[key] = req.body[key]));
         await loggedInUser.save();
-        res.send(`${loggedInUser.firstName}, your Profile Updated Successfully!`);
+        res.json({message : `${loggedInUser.firstName}, your Profile Updated Successfully!`, data : loggedInUser});
     }
     catch (err) {
         res.status(400).send("Error : " + err.message);
     }
 });
 
-module.exports = profileRouter;
+module.exports = profileRouter; 
